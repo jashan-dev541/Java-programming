e030a167bf245d8e4f86e73b3038f5ef4b3c7858
Jashanpreet Singh <itsjashan547@gmail.com>
2026-07-08 17:40:27 +0530

    I have to solve the questions yet but I'll update tonight

---
+++ arr.java
@@ -0,0 +1,10 @@
+public class arr {
+    public static void main(String args[]){
+        int arr[] = {5,4,3,8,7};
+        int sum = 0;
+        for(int i=0; i<5; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
---
+++ array.java
@@ -0,0 +1,33 @@
+// import java.util.*;
+// public class array {
+//     public static void main(String args[]){
+//         int n, even=0, odd=0;
+//         System.out.println("Enter no. of elements: ");
+//         Scanner sf = new Scanner(System.in);
+//         n = sf.nextInt();
+
+//         System.out.println("Enter the elements: ");
+//         int arr[] = new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i] = sf.nextInt();
+//         }
+
+//         //calculate no. of even and odd elements in array
+//         for(int i=0; i<n; i++){
+//             if(arr[i] %2 ==0) even++;
+//             else odd++;
+//         }
+//         System.out.println("Even elements: " + even + "\nOdd elements: " + odd);
+//         sf.close();
+//     }
+// }
+
+//Q. You have an array of size n. 
+//   Swap and print all the elements of the array (array reverse)
+
+//Q. You have an array of size n, and a target value t.
+// Check if the array contains a possible pair of the elements
+// such that their sum is equal to t. Print yes if it has
+// else print not.
+
+//Answer 1.
